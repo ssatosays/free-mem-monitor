7464d97b2850d071e7bcdbc5a4f6fd43d30defbd
ssatosays <ssatosays@gmail.com>
2023-08-05 19:00:20 +0900

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+output_csv='output.csv'
+interval=${INTERVAL:-1}
+
+function handle_sigint() {
+    cat ${output_csv}
+    exit 1
+}
+
+trap handle_sigint SIGINT
+
+function echo_free_mem_status() {
+    local date=$(date --iso-8601=seconds)
+    local free_mem=$(free -h | awk '/^Mem:/{print $4}')
+    echo "${date},${free_mem}"
+}
+
+echo 'date,free-mem' > ${output_csv}
+while true; do
+    echo_free_mem_status >> ${output_csv}
+    sleep ${interval}
+done
